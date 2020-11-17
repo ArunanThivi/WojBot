a1525910c40620d860385af5f51029ab7d38074c
Arunan Thiviyanathan <arunankogulan@gmail.com>
2020-11-17 14:43:43 -0600

    Adjusted to make API request every 2000 ms

--- index.js
+++ index.js
@@ -25,45 +25,48 @@ bot.on('message', message => {
 
 async function getRequest() {
 
-    // Edit query parameters below
     const params = {
-        'query': 'sources from:50323173 -is:retweet',
+        'query':"sources from:50323173 -is:retweet",
     } 
 
     const res = await needle('get', endpointURL, params, { headers: {
         "authorization": `Bearer ${twitterToken}`
     }})
 
-    if(res.body) {
+    /*if(res.body) {
         return res.body;
     } else {
         throw new Error ('Unsuccessful request')
-    }
-}
+    }*/
 
-(async () => {
 
-    try {
-        const response = await getRequest();
-        var data = response.data;
-        if (response.meta.newest_id > newest_id) {
-            for (tweet of data) {
-                if (tweet.id > newest_id) {
-                    tweets.push(tweet.id)
+    var timeOutPromise = new Promise(function(resolve, reject) {
+        // 2 Second delay
+        setTimeout(resolve, 2000, 'Timeout Done');
+    });
+
+    Promise.all([res, timeOutPromise]).then(function(values) {
+        try {
+            var data = values[0].body.data;
+            if (values[0].body.meta.newest_id > newest_id) {
+                for (tweet of data) {
+                    if (tweet.id > newest_id) {
+                        tweets.push(tweet.id)
+                    }
                 }
+                newest_id = values[0].body.meta.newest_id;
             }
-            newest_id = response.meta.newest_id;
+        } catch(e) {
+            console.log(e);
         }
-
-    } catch(e) {
-        console.log(e);
-    }
-  })();
-bot.login(discordToken);
+        console.log(tweets);
+        getRequest();
+    });
+};
 getRequest();
+bot.login(discordToken);
 /** 
 50323173 - @wojespn twitter ID
 178580925 - @Shams twitter ID
 "sources from:50323173" <- search rule
-
 */ 
