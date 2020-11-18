1a462a1588c2d432015595e6912caf0757080bd3
Arunan Thiviyanathan <arunankogulan@gmail.com>
2020-11-17 20:21:20 -0600

    added more sources to monitor and added support for multiple channels.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var events = require('events'); // EventEmitter Handler
 const Discord = require('discord.js'); // Discord Bot API
-var Twitter = require('twitter'); // Twitter API
-var needle = require('needle'); // REST API handler
+var needle = require('needle'); // Twitter API handler
 const {discordToken, twitterToken, endpointURL} = require('./config.json'); // API tokens
 
 const bot = new Discord.Client();
@@ -16,40 +14,61 @@ bot.once('ready', () => {
  * NEWEST_ID = Holds the id of the most recent tweet recieved. 
  */
 var latest = {
-    "woj": "1328619251117252608",
-    "shams": "1328795384890728448"
+    "haynes": "1328796295356551170",
+    "koc": "1328836478194114562",
+    "shams": "1328795384890728448",
+    "stein": "1328780236025180161",
+    "woj": "1328619251117252608"
 }
 const sources = {
-    "woj": "50323173",
-    "shams": "178580925"
+    "haynes": "57710919",
+    "koc": "35355156",
+    "shams": "178580925",
+    "stein": "48488561",
+    "woj": "50323173"    
 }
 /**
  * TWEETS = Holds a list of tweets that have been fetched from API but not yet published in server
  */
 var tweets = [];
 
-var channelID = "778169140455145472";
+var channelIDs = ["778169140455145472"];
 
 bot.on('tweet', tweets => {
-   // if (channelID != null) {    
-        var channel = bot.channels.fetch(channelID);
+    console.log(channelIDs);
+    for (c of channelIDs) {    
+        var channel = bot.channels.fetch(c);
         channel.then(function(result) {
-            let tweet = tweets.pop();
-            if (tweet.author_id == sources["shams"]) {
-                result.send('\n**SHAMS BOMB**\n\nhttps://twitter.com/ShamsCharania/status/' + tweet.id);
-            } else if (tweet.author_id == sources["woj"]) {
-                result.send('\n**WOJ BOMB**\n\nhttps://twitter.com/wojespn/status/' + tweet.id);
+            for (tweet of tweets) {
+                switch (tweet.author_id) {
+                    case sources["haynes"]:
+                        result.send("\n**New Tweet from Chris Haynes**\n\nhttps://twitter.com/ChrisBHaynes/status/" + tweet.id);
+                        break;
+                    case sources["koc"]:
+                        result.send("\n**New Tweet from Kevin O'Connor**\n\nhttps://twitter.com/KevinOConnorNBA/status/" + tweet.id);
+                        break;
+                    case sources["shams"]:
+                        result.send('\n**SHAMS BOMB**\n\nhttps://twitter.com/ShamsCharania/status/' + tweet.id);
+                        break;
+                    case sources["stein"]:
+                        result.send('\n**New Tweet from Marc Stein**\n\nhttps://twitter.com/ShamsCharania/status/' + tweet.id);
+                        break;
+                    case sources["woj"]:
+                        result.send('\n**WOJ BOMB**\n\nhttps://twitter.com/TheSteinLine/status/' + tweet.id);
+                        break;
+                
+                    default:
+                        break;
+                }
             }
-            });
-    //} else {
-   //     console.log("Channel not set! Use !here to set a channel!");
-   // }
+        });
+    }
 	
 });
 
 bot.on('message', message => {
     if (message.content === "!here") {
-        channelID = message.channel.id;
+        channelIDs.push(message.channel.id);
     }
 })
 
@@ -58,20 +77,25 @@ bot.on('message', message => {
  */
 async function getRequest() {
 
+    tweets = []; //Empty Array
+
     var timeOutPromise = new Promise(function(resolve, reject) {
-        // 2 Second delay
-        setTimeout(resolve, 4000, 'Timeout Done');
+        // 10 Second delay
+        setTimeout(resolve, 10000, 'Timeout Done');
     });
 
-    Promise.all([getTweets("woj"), getTweets("shams"), timeOutPromise]).then(function(values) {
+    Promise.all([getTweets("haynes"), getTweets("koc"), getTweets("shams"), getTweets("stein"), getTweets("woj"), timeOutPromise]).then(function(values) {
         try {
-            collectData(values[0].body, "woj");
-            collectData(values[1].body, "shams");
+            collectData(values[0].body, "haynes");
+            collectData(values[1].body, "koc");
+            collectData(values[2].body, "shams");
+            collectData(values[3].body, "stein");
+            collectData(values[4].body, "woj");
         } catch(e) {
             console.log(e);
         }
         if (tweets.length > 0) {
-            tweets.sort((a, b) => (a.created_at < b.created_at) ?  1 : -1); //Sort tweets chronoc
+            tweets.sort((a, b) => (a.created_at > b.created_at) ?  1 : -1); //Sort tweets chronoc
             bot.emit('tweet', tweets);
         }
         getRequest(); //Repeat Call
