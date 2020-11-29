f341555318b44117e9936f6340c1cd32d207c30a
Arunan Thiviyanathan <arunankogulan@gmail.com>
2020-11-29 16:59:14 -0600

    Final Release

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js'); // Discord Bot API
 var Monitor = require('monitor-twitter');
-const {discordToken, consumer_key, consumer_secret, access_token, access_token_secret} = require('./keys.json');
+const { discordToken, consumer_key, consumer_secret, access_token, access_token_secret } = require('./keys.json');
 
 var config = {
   consumer_key: consumer_key,
   consumer_secret: consumer_secret,
   access_token: access_token,
-  access_token_secret: access_token_secret 
+  access_token_secret: access_token_secret
 };
 
 var m = new Monitor(config);
@@ -16,83 +16,53 @@ var channelIDs = ["778169140455145472", "778799485131161640", "74620996666235289
 
 bot.once('ready', () => {
   console.log('Ready!');
-  bot.user.setActivity('NBA Draft Night', { type: 'WATCHING' })
-  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-  .catch(console.error);
+  bot.user.setActivity('NBA Free Agency', { type: 'WATCHING' })
+    .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+    .catch(console.error);
 });
 
 bot.login(discordToken);
 
 bot.on('message', message => {
   if (message.content === "|here") {
-      if (channelIDs[message.channel.id] == null) {
-          channelIDs.push(message.channel.id);
-      }
+    if (channelIDs[message.channel.id] == null) {
+      channelIDs.push(message.channel.id);
+    }
   }
   if (message.content === "|channels") {
-      console.log(channelIDs);
+    console.log(channelIDs);
   }
   if (message.content === "|remove") {
-      if (channelIDs[message.channel.id] == null) {
-          channelIDs.splice(channelIDs.indexOf(message.channel.id), 1);
-      }
+    if (channelIDs[message.channel.id] == null) {
+      channelIDs.splice(channelIDs.indexOf(message.channel.id), 1);
+    }
   }
 })
 
 // Watch the account 'wojespn' for Tweets containing 'source' every 30 seconds.
 
-m.start('wojespn', 'source', 30 * 1000);
 
-m.on('wojespn', function(tweet) {
+m.on('wojespn', function (tweet) {
   for (c of channelIDs) {
     var channel = bot.channels.fetch(c);
-    channel.then(function(result) {
+    channel.then(function (result) {
       result.send("__New Tweet from **Adrian Wojnarowski**__\n\n https://twitter.com/wojespn/status/" + tweet.id);
     }).catch(console.error);
   }
   console.log('WOJ BOMB', tweet);
 });
 
-m.start('ShamsCharania', 'source', 30 * 1000);
 
-m.on('ShamsCharania', function(tweet) {
+
+m.on('ShamsCharania', function (tweet) {
   for (c of channelIDs) {
     var channel = bot.channels.fetch(c);
-    channel.then(function(result) {
-        result.send("__New Tweet from **Shams Charania**__\n\n https://twitter.com/ShamsCharania/status/" + tweet.id);
+    channel.then(function (result) {
+      result.send("__New Tweet from **Shams Charania**__\n\n https://twitter.com/ShamsCharania/status/" + tweet.id);
     }).catch(console.error);
   }
+  console.log('SHAMS BOMB', tweet);
 });
 
-m.start('ChrisBHaynes', 'source', 30 * 1000);
-
-m.on('ChrisBHaynes', function(tweet) {
-  for (c of channelIDs) {
-    var channel = bot.channels.fetch(c);
-    channel.then(function(result) {
-        result.send("__New Tweet from **Chris Haynes**__\n\n https://twitter.com/ChrisBHaynes/status/" + tweet.id);
-    }).catch(console.error);
-  }
-});
-
-m.start('TheSteinLine', 'source', 30 * 1000);
-
-m.on('TheSteinLine', function(tweet) {
-  for (c of channelIDs) {
-    var channel = bot.channels.fetch(c);
-    channel.then(function(result) {
-        result.send("__New Tweet from **Marc Stein**__\n\n https://twitter.com/TheSteinLine/status/" + tweet.id);
-    }).catch(console.error);
-  }
-});
-
-m.start('KevinOConnorNBA', 'source', 30 * 1000);
-
-m.on('KevinOConnorNBA', function(tweet) {
-  for (c of channelIDs) {
-    var channel = bot.channels.fetch(c);
-    channel.then(function(result) {
-        result.send("__New Tweet from **Kevin O'Connor**__\n\n https://twitter.com/KevinOConnorNBA/status/" + tweet.id);
-    }).catch(console.error);
-  }
-});
+m.start('wojespn', 'source', 10 * 1000);
+m.start('ShamsCharania', 'source', 10 * 1000);
